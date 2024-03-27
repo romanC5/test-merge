93199d0b082a089ef2d2851dd8935f095dc043e0
Your Name <you@example.com>
2024-03-27 15:22:42 +0545

    movie first commit

---
+++ movie/src/App.jsx
@@ -0,0 +1,22 @@
+
+// import './App.css'
+
+
+import React from 'react'  
+import Header from './Header'
+import Footer from './Footer'
+import Contact from './Contact'
+const App = () => {
+  return (
+    <>
+   <Header/>
+   <Contact/>
+
+   <Footer/>
+   </>
+   
+  )
+}
+
+export default App
+
---
+++ movie/src/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>contact us</h1>
+       Email :
+
+    </div>
+  )
+}
+
+export default Contact
